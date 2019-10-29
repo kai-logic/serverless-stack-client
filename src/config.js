2e718b831a03e5c464a7f4ec48c30b63856f2684
Kai Andritzke <kandritzke@logicmedia.de>
2019-10-29 13:06:29 +0100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-central-1",
+    BUCKET: "kai-notesapp-uploads"
+  },
+  apiGateway: {
+    REGION: "eu-central-1",
+    URL: "https://3pdj7iutie.execute-api.eu-central-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-central-1",
+    USER_POOL_ID: "eu-central-1_vpTE13mRF",
+    APP_CLIENT_ID: "2blrio0b4tjqts240dffgsn77p",
+    IDENTITY_POOL_ID: "eu-central-1:a59c427d-a844-4e85-9db4-d7ef6b29f68e"
+  }
+};
